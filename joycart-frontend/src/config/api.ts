e36ee2a53df29abff13e411e5d8b0afad67f569a
HuacongXie <solarsys@outlook.com>
2025-09-18 19:04:30 -0400

    created api.ts to handle common path

---
+++ joycart-frontend/src/config/api.ts
@@ -0,0 +1,10 @@
+// API config, prefix
+export const API_CONFIG = {
+    BASE_URL: 'http://localhost:8083',
+};
+
+// API endpoints
+export const API_ENDPOINTS = {
+    REGISTER: '/users/register',
+    LOGIN: '/users/login',
+};
