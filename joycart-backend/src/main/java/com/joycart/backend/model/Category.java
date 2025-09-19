dcf87b8f8c096fcbc940ed0a66654c76a96bacb8
HuacongXie <solarsys@outlook.com>
2025-09-19 18:35:48 -0400

    created Category model

---
+++ joycart-backend/src/main/java/com/joycart/backend/model/Category.java
@@ -0,0 +1,78 @@
+package com.joycart.backend.model;
+
+import javax.persistence.*;
+
+/**
+ * 商品分类实体类
+ */
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column
+    private String imgUrl;
+    
+    @Column
+    private String description;
+    
+    @Column
+    private boolean isActive = true;
+
+
+    public Category() {
+    }
+
+    public Category(String name, String imgUrl, String description) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.description = description;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
