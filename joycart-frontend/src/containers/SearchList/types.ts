bf32f34ef930f7be869f1aaaa131caa45150be07
HuacongXie <solarsys@outlook.com>
2025-09-20 19:45:56 -0400

    added keyword to SearchList page's ResponseType

--- joycart-frontend/src/containers/SearchList/types.ts
+++ joycart-frontend/src/containers/SearchList/types.ts
@@ -3,7 +3,6 @@ export type ResponseType = {
     success: boolean,
     data: Array<{
         id: string;
-        keyword:string;
         imgUrl: string;
         title:string;
         price: number;
