d8d7cae3179230a45fe00bb5efc1b1bf25946101
HuacongXie <solarsys@outlook.com>
2025-09-21 01:08:18 -0400

    optimize

--- joycart-frontend/src/containers/Cart/Cart.tsx
+++ joycart-frontend/src/containers/Cart/Cart.tsx
@@ -130,7 +130,7 @@ function Cart() {
             method: 'POST',
             data: params
         }).then(response=>{
-            message('Please Confirm Your Order!');
+            // message('Please Confirm Your Order!');
             //if success, get orderId and navigate to order page
             const {orderId} = response.data;
             navigate(`/order/${orderId}`);
