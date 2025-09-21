faf690bb1e98d02d120a8fcdb5d635b82b82343a
HuacongXie <solarsys@outlook.com>
2025-09-20 20:42:57 -0400

    modified Profile  types.ts ResponseType

--- joycart-frontend/src/containers/Profile/types.ts
+++ joycart-frontend/src/containers/Profile/types.ts
@@ -1,4 +1,16 @@
 
+//用户资料响应类型
 export type ResponseType = {
     success: boolean;
+    message: string;
+    data: {
+        id: string;
+        nickname: string;
+        avatar: string;
+        vipLevel: string;
+        coupons: number;
+        rewardPoints: number;
+        phoneNumber: string;
+        email: string;
+    };
 }
