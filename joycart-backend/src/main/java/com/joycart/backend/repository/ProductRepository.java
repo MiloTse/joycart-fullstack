b75eb5a1787ac7c4ba57ed15b996f71dc38eb0a3
HuacongXie <solarsys@outlook.com>
2025-09-26 01:07:25 -0400

    created ProductRepository

---
+++ joycart-backend/src/main/java/com/joycart/backend/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.joycart.backend.repository;
+
+import com.joycart.backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+    /**
+     * 根据productId查找激活的商品
+     * @param productId 商品ID
+     * @return 商品信息
+     */
+    Optional<Product> findByProductIdAndIsActiveTrue(String productId);
+}
