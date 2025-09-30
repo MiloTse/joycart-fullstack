bd8b62ff3bb85301c77c9facad8f0ae17c5f60a9
HuacongXie <solarsys@outlook.com>
2025-09-29 23:46:14 -0400

    created interface CartRepository

---
+++ joycart-backend/src/main/java/com/joycart/backend/repository/CartRepository.java
@@ -0,0 +1,14 @@
+package com.joycart.backend.repository;
+
+import com.joycart.backend.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    
+
+    Optional<Cart> findByUserIdAndProductIdAndIsActiveTrue(Integer userId, String productId);
+}
