d58ddf82769d936f0b20e622e4d30a29e7f8b657
HuacongXie <solarsys@outlook.com>
2025-09-30 01:45:31 -0400

    added method findByUserIdAndIsActiveTrue

--- joycart-backend/src/main/java/com/joycart/backend/repository/CartRepository.java
+++ joycart-backend/src/main/java/com/joycart/backend/repository/CartRepository.java
@@ -4,6 +4,7 @@ import com.joycart.backend.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     
 
     Optional<Cart> findByUserIdAndProductIdAndIsActiveTrue(Integer userId, String productId);
+    
+    List<Cart> findByUserIdAndIsActiveTrue(Integer userId);
 }
