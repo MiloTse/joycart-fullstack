fc614b5967b082afe2fe2c5d9d7df966a80be685
HuacongXie <solarsys@outlook.com>
2025-10-02 21:16:33 -0400

    created method findByIsActiveTrue

--- joycart-backend/src/main/java/com/joycart/backend/repository/ProductRepository.java
+++ joycart-backend/src/main/java/com/joycart/backend/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.joycart.backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
      * @return 商品信息
      */
     Optional<Product> findByProductIdAndIsActiveTrue(String productId);
+    
+    /**
+     * 查找所有激活的商品
+     * @return 激活的商品列表
+     */
+    List<Product> findByIsActiveTrue();
 }
