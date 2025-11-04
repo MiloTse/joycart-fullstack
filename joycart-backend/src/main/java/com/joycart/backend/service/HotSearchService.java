559e6d2aa1a25574b2cc23bfb63f19719de5ec80
HuacongXie <solarsys@outlook.com>
2025-11-04 16:37:16 -0500

    modified HotSearchService

--- joycart-backend/src/main/java/com/joycart/backend/service/HotSearchService.java
+++ joycart-backend/src/main/java/com/joycart/backend/service/HotSearchService.java
@@ -7,16 +7,18 @@ public interface HotSearchService {
     
     /**
      * 获取所有激活的热门搜索列表
+     * @param languageCode 语言代码 (en-US, zh-CN, fr-FR)
      * @return 热门搜索列表
      */
-    List<Map<String, String>> getAllActiveHotSearches();
+    List<Map<String, String>> getAllActiveHotSearches(String languageCode);
     
     /**
      * 根据搜索ID获取热门搜索信息
      * @param searchId 搜索ID
+     * @param languageCode 语言代码 (en-US, zh-CN, fr-FR)
      * @return 热门搜索信息
      */
-    Map<String, String> getHotSearchById(String searchId);
+    Map<String, String> getHotSearchById(String searchId, String languageCode);
     
     /**
      * 增加搜索次数
